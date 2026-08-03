e175c9d7b1d2ef417a4c6ce398a0077e73cb0c8a
adityathok <aditya0thok@gmail.com>
2026-08-03 09:38:36 +0700

    psr 4

--- elearning-vd.php
+++ elearning-vd.php
@@ -19,30 +19,9 @@ define('ELVD_PLUGIN_FILE', __FILE__);
 define('ELVD_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('ELVD_REST_NAMESPACE', 'elvd/v1');
 
-foreach (
-    [
-        ELVD_PLUGIN_DIR . 'inc/*.php',
-        ELVD_PLUGIN_DIR . 'inc/db/*.php',
-        ELVD_PLUGIN_DIR . 'inc/post-provider/*.php',
-        ELVD_PLUGIN_DIR . 'inc/restapi/*.php',
-        ELVD_PLUGIN_DIR . 'inc/shortcodes/*.php',
-    ] as $elvd_pattern
-) {
-    foreach (glob($elvd_pattern) as $elvd_file) {
-        require_once $elvd_file;
-    }
-}
+require_once ELVD_PLUGIN_DIR . 'vendor/autoload.php';
 
-register_activation_hook(__FILE__, 'elvd_activate');
-register_deactivation_hook(__FILE__, 'elvd_deactivate');
+register_activation_hook(__FILE__, [ElearningVD\Plugin::class, 'activate']);
+register_deactivation_hook(__FILE__, [ElearningVD\Plugin::class, 'deactivate']);
 
-add_action('init', 'elvd_register_roles');
-add_action('init', 'elvd_register_post_types');
-add_action('init', 'elvd_register_post_meta');
-add_action('rest_api_init', 'elvd_register_rest_routes');
-add_action('wp_enqueue_scripts', 'elvd_register_frontend_assets');
-
-add_filter('theme_page_templates', 'elvd_register_page_template', 10, 4);
-add_filter('template_include', 'elvd_load_page_template');
-
-add_shortcode('elvd_app', 'elvd_app_shortcode');
+ElearningVD\Plugin::boot();
---
+++ src/Plugin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ElearningVD;
+
+defined('ABSPATH') || exit;
+
+final class Plugin
+{
+    public static function boot(): void
+    {
+        add_action('init', 'elvd_register_roles');
+        add_action('init', 'elvd_register_post_types');
+        add_action('init', 'elvd_register_post_meta');
+        add_action('rest_api_init', 'elvd_register_rest_routes');
+        add_action('wp_enqueue_scripts', 'elvd_register_frontend_assets');
+
+        add_filter('theme_page_templates', 'elvd_register_page_template', 10, 4);
+        add_filter('template_include', 'elvd_load_page_template');
+
+        add_shortcode('elvd_app', 'elvd_app_shortcode');
+    }
+
+    public static function activate(): void
+    {
+        elvd_activate();
+    }
+
+    public static function deactivate(): void
+    {
+        elvd_deactivate();
+    }
+}
