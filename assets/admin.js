d3a30350e9edc74e65a61633a97917d9eb3bbd8b
adityathok <aditya0thok@gmail.com>
2026-08-06 09:22:50 +0700

    feat: admin js

---
+++ assets/admin.js
@@ -0,0 +1,80 @@
+jQuery(function ($) {
+    var frame;
+    var $logoId = $('#elvd-school-logo-id');
+    var $preview = $('#elvd-school-logo-preview');
+    var $removeButton = $('#elvd-remove-school-logo');
+
+    $('#elvd-select-school-logo').on('click', function (event) {
+        event.preventDefault();
+
+        if (frame) {
+            frame.open();
+            return;
+        }
+
+        frame = wp.media({
+            title: 'Pilih Logo Sekolah',
+            button: {
+                text: 'Gunakan Logo'
+            },
+            multiple: false
+        });
+
+        frame.on('select', function () {
+            var attachment = frame.state().get('selection').first().toJSON();
+            var imageUrl = attachment.sizes && attachment.sizes.thumbnail
+                ? attachment.sizes.thumbnail.url
+                : attachment.url;
+
+            $logoId.val(attachment.id);
+            $preview.attr('src', imageUrl).removeClass('hidden');
+            $removeButton.removeClass('hidden');
+        });
+
+        frame.open();
+    });
+
+    $removeButton.on('click', function (event) {
+        event.preventDefault();
+
+        $logoId.val('');
+        $preview.attr('src', '').addClass('hidden');
+        $removeButton.addClass('hidden');
+    });
+
+    $('.elvd-add-profile-field').on('click', function (event) {
+        event.preventDefault();
+
+        var option = $(this).data('field-option');
+        var $tbody = $($(this).data('target') + ' tbody');
+        var index = $tbody.find('tr').length;
+        var row = ''
+            + '<tr>'
+            + '<td><input type="text" name="' + option + '[' + index + '][key]" value="" class="regular-text" required></td>'
+            + '<td><input type="text" name="' + option + '[' + index + '][label]" value="" class="regular-text" required></td>'
+            + '<td><select name="' + option + '[' + index + '][type]">'
+            + '<option value="text">Text</option>'
+            + '<option value="email">Email</option>'
+            + '<option value="number">Angka</option>'
+            + '<option value="date">Tanggal</option>'
+            + '<option value="tel">Telepon</option>'
+            + '<option value="textarea">Textarea</option>'
+            + '</select></td>'
+            + '<td><select name="' + option + '[' + index + '][target]">'
+            + '<option value="meta">User Meta</option>'
+            + '<option value="display_name">Nama Tampilan User</option>'
+            + '<option value="user_email">Email User</option>'
+            + '</select></td>'
+            + '<td><label><input type="checkbox" name="' + option + '[' + index + '][required]" value="1"> Ya</label></td>'
+            + '<td><button type="button" class="button elvd-remove-profile-field">Hapus</button></td>'
+            + '</tr>';
+
+        $tbody.append(row);
+    });
+
+    $('.elvd-profile-fields').on('click', '.elvd-remove-profile-field', function (event) {
+        event.preventDefault();
+
+        $(this).closest('tr').remove();
+    });
+});
