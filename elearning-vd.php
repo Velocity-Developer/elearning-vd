7a4e02eb88dc274754dd6148a9c5e66acf0b46b3
adityathok <aditya0thok@gmail.com>
2026-08-08 10:08:39 +0700

    feat: version patch

--- elearning-vd.php
+++ elearning-vd.php
@@ -4,7 +4,7 @@
  * Plugin Name: Elearning VD
  * Plugin URI: https://velocitydeveloper.com/
  * Description: Plugin elearning untuk sekolah SD, SMP, dan SMA.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Velocity Developer
  * Author URI: https://velocitydeveloper.com/
  * Text Domain: elearning-vd
@@ -14,7 +14,7 @@
 
 define('ELVD_PLUGIN_FILE', __FILE__);
 define('ELVD_PLUGIN_URL', plugins_url('', __FILE__));
-define('ELVD_PLUGIN_VERSION', '1.0.0');
+define('ELVD_PLUGIN_VERSION', '1.0.1');
 
 defined('ABSPATH') || exit;
 
